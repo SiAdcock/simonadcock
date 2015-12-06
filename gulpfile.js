8e8184e79a1dcc6a141edfeb3bfd09de809dcd90
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-12-06 17:42:19 +0000

    Watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const lint = require('gulp-eslint');
+const scssPath = './library/scss/**/*.scss';
 
 gulp.task('sass', () => {
-  gulp.src('./library/scss/**/*.scss')
+  gulp.src(scssPath)
     .pipe(sass()
       .on('error', sass.logError))
     .pipe(gulp.dest('./build/css/'));
@@ -19,4 +20,7 @@ gulp.task('lint', () => {
     .pipe(lint())
     .pipe(lint.format())
     .pipe(lint.failOnError());
+});
+gulp.task('watch', () => {
+  gulp.watch(scssPath, ['sass']);
 });
