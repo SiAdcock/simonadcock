d0cf7b9f07a0d2d0db76942bee312d8d4147a058
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-12-06 17:35:56 +0000

    Set up linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
+'use strict';
+
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const lint = require('gulp-eslint');
 
 gulp.task('sass', () => {
   gulp.src('./library/scss/**/*.scss')
     .pipe(sass()
       .on('error', sass.logError))
     .pipe(gulp.dest('./build/css/'));
+});
+gulp.task('lint', () => {
+  return gulp.src(
+    [
+      './gulpfile.js',
+      './library/js/scripts.js'
+    ])
+    .pipe(lint())
+    .pipe(lint.format())
+    .pipe(lint.failOnError());
 });
