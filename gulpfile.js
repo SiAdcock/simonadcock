a56fa1131a56dce2e5d325db1d6a586b4b3d4f7f
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-12-06 17:08:32 +0000

    Auto generate CSS from SCSS

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', () => {
+  gulp.src('./library/scss/**/*.scss')
+    .pipe(sass()
+      .on('error', sass.logError))
+    .pipe(gulp.dest('./library/css/'));
+});
